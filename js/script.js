979b6e85d645c10999bf0f8dd17eb4aa6c785fd0
siddharthDyamgond <117370082+siddyamgond@users.noreply.github.com>
2022-11-04 22:03:34 +0530

    UI

--- js/script.js
+++ js/script.js
@@ -12,61 +12,78 @@ function getPokemon(e) {
   const name = document.querySelector("#text").value;
   const pokemonName = lowerCaseName(name);
 
-//   fetch(`https://pokeapi.co/api/v2/ability/${pokemonName}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-       
-//     });
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
       document.querySelector("#Pokecard").innerHTML = `
-      <div class="container p-3 bg-white">
-      <h1 class="text-center">Pokemon encyclopedia - ${capitalizeFirstLetter(data.name)}</h1>
-      <br />
-      <center>
-      
-      <img src="${data.sprites.other["official-artwork"].front_default}" class="img-fluid" alt="text" /></center>
-      <div class="container p-3">
-        <h4>ID: #${(data.id)}</h4>
+      <div
+        class="container"
+        style="
+          background-image: url(./img/test.png);
+          width: 100%;
+          
+          background-size: cover;
+        "
+      >
+      <div class="row pt-2">
+          <div class="row g-0 text-center">
+            <div class="col-sm-6 col-md-6"></div>
+            <div class="col-6 col-md-5">
+              <h2 class="text-end">
+                Pokedex - ${capitalizeFirstLetter(data.name)}
+              </h2>
+            </div>
+          </div>
+        </div>
         <br />
-        <br />
-        <h5>Description:</h5>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Base Experience</th>
-              <th scope="col">Height</th>
-              <th scope="col">Weight</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-            <td>${data.base_experience}</td>
-            <td>${data.weight}</td>
-            <td>${data.height}</td>
-            </tr>
-          </tbody>
-          <br />
-          <thead>
-            <tr>
-              <th scope="col">Base stat</th>
-              <th scope="col">Height</th>
-              <th scope="col">Weight</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>${data.base_stat}</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="row">
+          <div class="col-md">
+            <center>
+              <img src="${data.sprites.other["official-artwork"].front_default}"
+              class="img-fluid" height="500px" alt="text" />
+            </center>
+          </div>
+          <div class="col-md">
+            <div class="container p-3">
+              <h4>ID: #${(data.id)}</h4>
+              <h2 class="">
+                ${capitalizeFirstLetter(data.name)}
+              </h2>
+              <h5>Description:</h5>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Base Experience</th>
+                    <th scope="col">Height</th>
+                    <th scope="col">Weight</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>${data.base_experience}</td>
+                    <td>${data.weight}</td>
+                    <td>${data.height}</td>
+                  </tr>
+                </tbody>
+                <br />
+                <thead>
+                  <tr>
+                    <th scope="col">Base stat</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>${data.base_stat}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
 
     `;
     })
